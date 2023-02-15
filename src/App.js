3a8f859d6b762542c314b3b83a5c03342936321f
Ziad Tarek <ziadtarekfa@gmail.com>
2023-02-15 19:15:09 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <>
-      <SignUp />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </Router>
+
+
   );
 }
 
